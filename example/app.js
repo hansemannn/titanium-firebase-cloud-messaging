6a028b6ac7a0e2f9655a3228a3d3453d09b9c4e9
Hans Knöchel <hansemannn@users.noreply.github.com>
2018-04-06 12:33:31 +0200

    Update app.js

--- example/app.js
+++ example/app.js
@@ -1,7 +1,31 @@
-// Require the Firebase CloudMessaging module
-var FirebaseCloudMessaging = require('firebase.cloudmessaging');
+var core = require('firebase.core');
+var fcm = require('firebase.cloudmessaging');
 
-// Configure FirebaseCloudMessaging
-Ti.API.info('FCM-Token :' + FirebaseCloudMessaging.fcmToken);
+// Configure core module (required for all Firebase modules)
+core.configure();
 
-// TODO: Write more examples based from the Readme.
+// Called when the Firebase token is ready
+fcm.addEventListener('didRefreshRegistrationToken', onToken);
+
+// Called when direct messages arrive. Note that these are different from push notifications
+fcm.addEventListener('didReceiveMessage', onMessage);
+
+fcm.registerForPushNotifications();
+
+function onToken(e) {
+    Ti.API.info('Token', e.fcmToken);
+}
+
+function onMessage(e) {
+    Ti.API.info('Message', e.message);
+}
+
+// check if token is already available
+if (fcm.fcmToken !== null) {
+    Ti.API.info('FCM-Token', fcm.fcmToken);
+} else {
+    Ti.API.info('Token is empty. Waiting for the token callback ...');
+}
+
+// subscribe to topic
+fcm.subcribeToTopic('testTopic');

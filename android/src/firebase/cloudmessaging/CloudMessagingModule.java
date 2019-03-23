7557537b91a287ccee0e2879f001ff293c872157
hansemannn <hans.knoechel@hs-osnabrueck.de>
2019-03-23 13:03:46 +0100

    fix: re-enable push notifications service

--- android/src/firebase/cloudmessaging/CloudMessagingModule.java
+++ android/src/firebase/cloudmessaging/CloudMessagingModule.java
@@ -29,6 +29,8 @@ import org.appcelerator.titanium.util.TiConvert;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.RemoteMessage;
+import com.google.firebase.iid.InstanceIdResult;
+import com.google.android.gms.tasks.OnSuccessListener;
 import org.appcelerator.kroll.KrollDict;
 import java.util.HashMap;
 import org.appcelerator.kroll.KrollFunction;
@@ -91,6 +93,15 @@ public class CloudMessagingModule extends KrollModule
 	@Kroll.method
 	public void registerForPushNotifications()
 	{
+		FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(
+			TiApplication.getAppRootOrCurrentActivity(), new OnSuccessListener<InstanceIdResult>() {
+				@Override
+				public void onSuccess(InstanceIdResult instanceIdResult)
+				{
+					String newToken = instanceIdResult.getToken();
+					onTokenRefresh(newToken);
+				}
+		});
 		parseBootIntent();
 	}
 

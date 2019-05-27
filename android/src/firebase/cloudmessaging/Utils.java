32f9d00fd2eecddcd28b1c2052da01d7edef8ba4
Stian G. Gjerløw <stian@appivate.com>
2019-05-27 21:03:55 +0200

    Import dependencies

--- android/src/firebase/cloudmessaging/Utils.java
+++ android/src/firebase/cloudmessaging/Utils.java
@@ -1,6 +1,14 @@
 package firebase.cloudmessaging;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import org.appcelerator.titanium.TiApplication;
 
 public final class Utils

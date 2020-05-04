31c5e7165237c83e348935ee838ad0f2e4a95135
hansemannn <hans.knoechel@hs-osnabrueck.de>
2020-05-04 10:54:07 +0200

    chore: restore badger

--- android/src/firebase/cloudmessaging/TiFirebaseMessagingService.java
+++ android/src/firebase/cloudmessaging/TiFirebaseMessagingService.java
@@ -24,6 +24,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
+import me.leolin.shortcutbadger.ShortcutBadger;
 import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.titanium.TiApplication;
 import org.appcelerator.titanium.util.TiConvert;
@@ -273,6 +274,7 @@ public class TiFirebaseMessagingService extends FirebaseMessagingService
 
 		// Badge number
 		if (params.get("badge") != null && params.get("badge") != "") {
+			ShortcutBadger.applyCount(context, TiConvert.toInt(params.get("badge"), 1));
 			builder.setNumber(TiConvert.toInt(params.get("badge"), 1));
 		}
 

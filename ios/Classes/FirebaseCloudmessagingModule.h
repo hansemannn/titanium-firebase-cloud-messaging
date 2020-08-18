e0f5549b173ec23949a7b4ac6db3c7d5d533b17a
hansemannn <hans.knoechel@hs-osnabrueck.de>
2020-08-18 16:24:43 +0200

    feat(ios): update to Firebase 6.29.0

--- ios/Classes/FirebaseCloudmessagingModule.h
+++ ios/Classes/FirebaseCloudmessagingModule.h
@@ -6,23 +6,13 @@
  */
 
 #import "TiModule.h"
-
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
 #import <UserNotifications/UserNotifications.h>
-#endif
 
 @import FirebaseMessaging;
 
-#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-@interface FirebaseCloudmessagingModule : TiModule<FIRMessagingDelegate, UIApplicationDelegate, UNUserNotificationCenterDelegate> {
-#else
-@interface FirebaseCloudmessagingModule : TiModule<FIRMessagingDelegate, UIApplicationDelegate> {
-#endif
-}
+@interface FirebaseCloudmessagingModule : TiModule<FIRMessagingDelegate, UIApplicationDelegate, UNUserNotificationCenterDelegate>
 
 - (NSString *)fcmToken;
-
-- (void)registerForPushNotifications:(id)arguments;
 
 - (void)setApnsToken:(NSString *)apnsToken;
 

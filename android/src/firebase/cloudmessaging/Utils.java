25fb55a44c12dd64ce193bc4787e4faf2cb412ca
m1ga <migamiga@web.de>
2022-08-17 00:01:55 +0200

    feat(android): android 13 permission request

--- android/src/firebase/cloudmessaging/Utils.java
+++ android/src/firebase/cloudmessaging/Utils.java
@@ -9,91 +9,88 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
-import android.graphics.RectF;
 import android.net.Uri;
+
 import org.appcelerator.titanium.TiApplication;
 
-public final class Utils
-{
+public final class Utils {
 
-	/**
+    /**
      * Returns the identifier of the resource at the path res/<type>/<name>
+     *
      * @param type
      * @param name
      * @return
      */
-	public static int getResourceIdentifier(String type, String name)
-	{
-		int icon = 0;
-		if (name != null) {
-			int index = name.lastIndexOf(".");
-			if (index > 0) {
-				name = name.substring(0, index);
-			}
-			Context context = Utils.getApplicationContext();
-			icon = context.getResources().getIdentifier(name, type, context.getPackageName());
-		}
+    public static int getResourceIdentifier(String type, String name) {
+        int icon = 0;
+        if (name != null) {
+            int index = name.lastIndexOf(".");
+            if (index > 0) {
+                name = name.substring(0, index);
+            }
+            Context context = Utils.getApplicationContext();
+            icon = context.getResources().getIdentifier(name, type, context.getPackageName());
+        }
 
-		return icon;
-	}
+        return icon;
+    }
 
-	public static Uri getSoundUri(String name)
-	{
-		Context context = getApplicationContext();
-		return new Uri.Builder()
-			.scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
-			.authority(context.getPackageName())
-			.appendPath("raw")
-			.appendPath(name)
-			.build();
-	}
+    public static Uri getSoundUri(String name) {
+        Context context = getApplicationContext();
+        return new Uri.Builder()
+                .scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+                .authority(context.getPackageName())
+                .appendPath("raw")
+                .appendPath(name)
+                .build();
+    }
 
-	public static Context getApplicationContext()
-	{
-		return TiApplication.getInstance().getApplicationContext();
-	}
+    public static Context getApplicationContext() {
+        return TiApplication.getInstance().getApplicationContext();
+    }
 
-	/**
+    /**
      * Returns a circle shaped Bitmap of a square or rectangular input Bitmap
+     *
      * @param bitmap
      * @return
      */
-	public static Bitmap getCircleBitmap(Bitmap bitmap)
-	{
-		Bitmap output;
-		Rect srcRect, dstRect;
-		float r;
-		final int width = bitmap.getWidth();
-		final int height = bitmap.getHeight();
+    public static Bitmap getCircleBitmap(Bitmap bitmap) {
+        Bitmap output;
+        Rect srcRect, dstRect;
+        float r;
+        final int width = bitmap.getWidth();
+        final int height = bitmap.getHeight();
 
-		if (width > height) {
-			output = Bitmap.createBitmap(height, height, Bitmap.Config.ARGB_8888);
-			int left = (width - height) / 2;
-			int right = left + height;
-			srcRect = new Rect(left, 0, right, height);
-			dstRect = new Rect(0, 0, height, height);
-			r = height / 2;
-		} else {
-			output = Bitmap.createBitmap(width, width, Bitmap.Config.ARGB_8888);
-			int top = (height - width) / 2;
-			int bottom = top + width;
-			srcRect = new Rect(0, top, width, bottom);
-			dstRect = new Rect(0, 0, width, width);
-			r = width / 2;
-		}
+        if (width > height) {
+            output = Bitmap.createBitmap(height, height, Bitmap.Config.ARGB_8888);
+            int left = (width - height) / 2;
+            int right = left + height;
+            srcRect = new Rect(left, 0, right, height);
+            dstRect = new Rect(0, 0, height, height);
+            r = height / 2;
+        } else {
+            output = Bitmap.createBitmap(width, width, Bitmap.Config.ARGB_8888);
+            int top = (height - width) / 2;
+            int bottom = top + width;
+            srcRect = new Rect(0, top, width, bottom);
+            dstRect = new Rect(0, 0, width, width);
+            r = width / 2;
+        }
 
-		Canvas canvas = new Canvas(output);
+        Canvas canvas = new Canvas(output);
 
-		final Paint paint = new Paint(Color.TRANSPARENT);
-		paint.setAntiAlias(true);
+        final Paint paint = new Paint(Color.TRANSPARENT);
+        paint.setAntiAlias(true);
 
-		canvas.drawARGB(0, 0, 0, 0);
-		canvas.drawCircle(r, r, r, paint);
-		paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-		canvas.drawBitmap(bitmap, srcRect, dstRect, paint);
+        canvas.drawARGB(0, 0, 0, 0);
+        canvas.drawCircle(r, r, r, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, srcRect, dstRect, paint);
 
-		bitmap.recycle();
+        bitmap.recycle();
 
-		return output;
-	}
+        return output;
+    }
 }
